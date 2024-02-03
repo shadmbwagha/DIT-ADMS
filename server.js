3549703569036a3e390949f8f609de19db5b52dc
shad mbwagha <shadracksylvestar@gmail.com>
2024-02-03 08:06:31 +0300

    adms-pug

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser')
+
+const db =  mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'DIT_ADMS3'
+});
+
+db.connect((err)=> {
+    if(err){
+        throw err;
+    }
+    console.log('Mysql connected...');
+});
+
+//exress app
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use('/images', express.static('images'));
+
+app.listen(3000);
+
+
+app.get('/', ((req, res)=>{
+    db.query('SELECT * FROM departments', (err, rows) => {
+        if (err) {
+            console.error('MySQL query error:', err);
+            return res.status(500).send('Internal Server Error');
+        }
+        console.log(rows);
+        res.render('department.pug', { departments: rows });    
+    });  
+}));
+app.get('/departmentForm', ((req, res)=>{
+    db.query('SELECT * FROM departments', (err, rows) => {
+        if (err) {
+            console.error('MySQL query error:', err);
+            return res.status(500).send('Internal Server Error');
+        }
+        console.log(rows);
+        res.render('departmentForm.pug', { departments: rows });    
+    });  
+}));
+
